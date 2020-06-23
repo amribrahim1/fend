8fae2447245d470cbf62080f6f15690513ced220
amribrahim1 <amribrahim1@users.noreply.github.com>
2020-06-23 17:41:17 +0200

    Update app.js

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -17,8 +17,8 @@
  * Define Global Variables
  * 
 */
-
-
+const sections = document.querySelectorAll('section');
+const ul = document.getElementById('navbar__list');
 /**
  * End Global Variables
  * Start Helper Functions
@@ -32,12 +32,28 @@
  * Begin Main Functions
  * 
 */
+for (const section of sections) {  
+    // build the nav
+    const li = document.createElement('li'); 
+    ul.appendChild(li);   
+    const a = document.createElement('a'); 
+    a.setAttribute('href', `#${section.id}`);    
 
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
+    // Add class 'active' to section when near top of viewport
+    addEventListener('scroll', function onScroll() { 
+        var scrollPos = $(document).scrollTop();
+        $('.navbar__menu a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('.navbar__menu ul li a').removeClass("active");
+                currLink.addClass("active");
+            }
+            else{
+                currLink.removeClass("active");
+            }
+        });
+    })
 
 // Scroll to anchor ID using scrollTO event
 
@@ -50,8 +66,17 @@
 
 // Build menu 
 
-// Scroll to section on link click
-
-// Set sections as active
-
-
+    // Scroll to section on link click
+    a.addEventListener('click', function clickHandler() {                 
+        // remove active from another sections
+        const allLi = ul.querySelectorAll('a');      
+        allLi.forEach((a) => {               
+            a.classList.remove('active');     
+        })        
+        // Set section as active
+        this.classList.add('active');    
+    });
+    a.textContent = section.dataset.nav;    
+    li.appendChild(a);
+    
+};
